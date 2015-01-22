2f7169db2bfe282f4eb667c024ab2f848d295125
besil <s.bernardinello@reply.it>
2015-01-22 10:36:32 +0100

    Creating reader

---
+++ extraction/grapher.py
@@ -0,0 +1,39 @@
+import networkx as nx
+from numpy import genfromtxt
+from pprint import pprint
+
+def read(fname, sep=","):
+    fin = open(fname, 'r')
+    lines = [ x.strip().split(sep) for x in fin.readlines() ]
+    fin.close()
+    
+    split = lines[0]
+    header = { split[i]:i for i in range(len(split)) }
+    
+    
+    return ( header, lines[1:] )
+    
+
+if __name__ == '__main__':
+    anagr_fname = "anagrafiche.csv"
+    
+    data = read( anagr_fname, sep="," )
+    header, lines = read(anagr_fname)
+
+    from pprint import pprint
+    pprint(header)
+    
+    sinistro = header['SINISTRO']
+    fisc_altro = header['COD_FISC_COND_VEIC_CTP']
+    fisc_nostr = header['COD_FISC_CONTR_POL_NSP']
+    fiscale_altri   = set()
+    fiscali_nstr    = set()
+    
+    for line in lines:
+        fiscale_altri |= { line[fisc_altro] }
+        fiscali_nstr  |= { line[fisc_nostr] }
+    
+    print "Lines:", len(lines)
+    print "Fiscali altri:", len(fiscale_altri)
+    print "Fiscali nostri:", len(fiscali_nstr)
+    

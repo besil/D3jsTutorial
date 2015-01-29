db7c28a46f8813d30aa56ace6e7b89c315a5f5cf
besil <silvio.bernardinello@gmail.com>
2015-01-29 23:06:19 +0100

    Colorazione vicini

--- jsnxnet/main.js
+++ jsnxnet/main.js
@@ -10,7 +10,6 @@ G.add_edges_from([[1,3],[1,4],[3,4],[2,3],[2,4],[8,9],[8,10],[9,10],[11,10],[11,
 var color = d3.scale.category20();
 
 jsnx.draw(G, {
-	id : "mondo",
 	element: '#canvas',
 	layout_attr: {
 		charge: -120,
@@ -18,15 +17,33 @@ jsnx.draw(G, {
 	},
 	node_attr: {
 		r: 5,
-		title: function(d) { return d.label;}
+		title: function(d) { return d.label; },
+		id: function(d) { return "node-"+d.node; }
 	},
 	node_style: {
 		fill: function(d) { 
 			return color(d.data.group); 
 		},
-		stroke: 'none'
+		stroke: 'none',
 	},
 	edge_style: {
 		fill: '#999'
 	}
+}, true);
+
+function highlight_nodes(nodes, on) {
+    nodes.forEach(function(n) {
+        d3.select('#node-' + n).style('fill', function(d) {
+            return on ? '#EEE' : color( d.data.group );
+        });
+    });
+}
+
+// bind event handlers
+d3.selectAll('.node').on('mouseover', function(d) {
+    highlight_nodes(d.G.neighbors(d.node).concat(d.node), true);
+});
+
+d3.selectAll('.node').on('mouseout', function(d) {
+    highlight_nodes(d.G.neighbors(d.node).concat(d.node), false);
 });
